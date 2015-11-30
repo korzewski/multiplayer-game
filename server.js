40feb36ca642a9eac84e85e1e18b47d531f4b6cb
Rafal <rafal@nitro-digital.com>
2015-11-30 17:33:35 -0500

    heroku update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PeerServer = require('peer').PeerServer;
+//var PeerServer = require('peer').PeerServer;
 
 app.set('port', (process.env.PORT || 5000));
 
