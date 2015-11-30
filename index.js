7f26341fb51294167a70b4a248193766b3857bb4
Rafal <rafal@nitro-digital.com>
2015-11-30 17:43:09 -0500

    heroku update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-//var PeerServer = require('peer').PeerServer;
+var PeerServer = require('peer').PeerServer;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,30 +13,30 @@ console.log('server is running!');
 //});
 
 var expressServer = app.listen( app.get('port') );
-//var io = require('socket.io').listen(expressServer);
+var io = require('socket.io').listen(expressServer);
 
-//var peerServer = new PeerServer({ port: 9000, path: '/multiplayer' });
-//var allConnectedPeers = [];
-//
-//
-//peerServer.on('connection', function(peerID){
-//	allConnectedPeers.push( { peerID: peerID } );
-//	io.emit('user-connected', peerID);
-//
-//	console.log('new user connected: ', peerID);
-//});
-//
-//peerServer.on('disconnect', function(peerID){
-//	allConnectedPeers.forEach((peer, index) =>{
-//		if(peer.peerID == peerID){
-//			allConnectedPeers.splice(index, 1);
-//		}
-//	});
-//	io.emit('user-disconnected', peerID);
-//
-//	console.log('user disconnected: ', peerID);
-//});
-//
-//app.get('/api/allConnectedPeers', function(req, res){
-//	return res.json(allConnectedPeers);
-//});
+var peerServer = new PeerServer({ port: 9000, path: '/multiplayer' });
+var allConnectedPeers = [];
+
+
+peerServer.on('connection', function(peerID){
+	allConnectedPeers.push( { peerID: peerID } );
+	io.emit('user-connected', peerID);
+
+	console.log('new user connected: ', peerID);
+});
+
+peerServer.on('disconnect', function(peerID){
+	allConnectedPeers.forEach((peer, index) =>{
+		if(peer.peerID == peerID){
+			allConnectedPeers.splice(index, 1);
+		}
+	});
+	io.emit('user-disconnected', peerID);
+
+	console.log('user disconnected: ', peerID);
+});
+
+app.get('/api/allConnectedPeers', function(req, res){
+	return res.json(allConnectedPeers);
+});
