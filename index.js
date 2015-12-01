0f2d76c43b46732101b207ff651d1a76cc0d7ad4
Rafal <rafal@nitro-digital.com>
2015-11-30 21:24:44 -0500

    heroku update

--- index.js
+++ index.js
@@ -2,38 +2,38 @@ var express = require('express');
 var app = express();
 var PeerServer = require('peer').PeerServer;
 
-var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//var allowCrossDomain = function(req, res, next) {
+//	res.header('Access-Control-Allow-Origin', '*');
+//	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//
+//	// intercept OPTIONS method
+//	if ('OPTIONS' == req.method) {
+//		res.send(200);
+//	}
+//	else {
+//		next();
+//	}
+//};
+//
+//app.use(allowCrossDomain);
 
-	// intercept OPTIONS method
-	if ('OPTIONS' == req.method) {
-		res.send(200);
-	}
-	else {
-		next();
-	}
-};
-
-app.use(allowCrossDomain);
-
-app.set('port', (process.env.PORT || 5000));
+//app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static( __dirname + '/build' ));
 
-var expressServer = app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+//var expressServer = app.listen(app.get('port'), function() {
+//    console.log('Node app is running on port', app.get('port'));
+//});
 
-var io = require('socket.io').listen(expressServer);
+//var io = require('socket.io').listen(expressServer);
 
 var peerServer = new PeerServer({ port: 9000, path: '/build' });
 var allConnectedPeers = [];
 
 peerServer.on('connection', function(peerID){
 	allConnectedPeers.push( { peerID: peerID } );
-	io.emit('user-connected', peerID);
+	//io.emit('user-connected', peerID);
 
 	console.log('new user connected: ', peerID);
 });
@@ -44,7 +44,7 @@ peerServer.on('disconnect', function(peerID){
 			allConnectedPeers.splice(index, 1);
 		}
 	});
-	io.emit('user-disconnected', peerID);
+	//io.emit('user-disconnected', peerID);
 
 	console.log('user disconnected: ', peerID);
 });
